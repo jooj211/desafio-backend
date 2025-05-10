a6e38407ffc29f73c9ab1b9effbf57ec1cab3298
Jonatas Dias <diasjonatas003@gmail.com>
2025-05-10 16:34:41 -0300

    Updating database files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
 import { UsinasModule } from './modules/usinas/usinas.module';
 import { InversoresModule } from './modules/inversores/inversores.module';
 import { MetricasModule } from './modules/metricas/metricas.module';
 
 @Module({
-  imports: [UsinasModule, InversoresModule, MetricasModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    DatabaseModule,
+    UsinasModule,
+    InversoresModule,
+    MetricasModule
+  ],
 })
 export class AppModule {}
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,24 +1,24 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { Usina } from "src/modules/usinas/entities/usina.entity/usina.entity";
-import { Inversor } from "src/modules/inversores/entities/inversor.entity/inversor.entity";
-import { Leitura } from "src/modules/metricas/entities/leitura.entity/leitura.entity";
+import 'dotenv/config';
 
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Usina } from 'src/modules/usinas/entities/usina.entity/usina.entity';
+import { Inversor } from 'src/modules/inversores/entities/inversor.entity/inversor.entity';
+import { Leitura } from 'src/modules/metricas/entities/leitura.entity/leitura.entity';
 
 @Module({
-    imports: [
-        TypeOrmModule.forRoot({
-            type: 'postgres',
-            host: 'localhost',
-            port: 5432, 
-            username: 'postgres',
-            password: 'senha',
-            database: 'banco', 
-            entities: [Usina, Inversor, Leitura],
-            synchronize: true,
-        }),
-        TypeOrmModule.forFeature([Usina, Inversor, Leitura])
-    ],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT!, 10),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [Usina, Inversor, Leitura],
+      synchronize: false, // use migrations em dev
+    }),
+  ],
+  exports: [TypeOrmModule],
 })
-
-export class AppModule {}
+export class DatabaseModule {}
