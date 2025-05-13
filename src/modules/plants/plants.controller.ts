e9951ab82a522bcd40a89cfb8f890065b89fb36a
Jonatas Dias <diasjonatas003@gmail.com>
2025-05-13 08:32:16 -0300

    Adding missing Plants controller

--- src/modules/plants/plants.controller.ts
+++ src/modules/plants/plants.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('plants')
-export class PlantsController {}
+import {
+    Controller,
+    Get,
+    Post,
+    Patch,
+    Delete,
+    Param,
+    Body,
+    HttpCode,
+    HttpStatus,
+    ParseIntPipe,
+  } from '@nestjs/common';
+  import { PlantsService } from './plants.service';
+  import { CreatePlantDto } from './dtos/create-plant.dto';
+  import { UpdatePlantDto } from './dtos/update-plant.dto';
+  
+  @Controller('plants')
+  export class PlantsController {
+    constructor(private readonly plantsService: PlantsService) {}
+  
+    @Get()
+    findAll() {
+      return this.plantsService.findAll();
+    }
+  
+    @Get(':id')
+    findOne(@Param('id', ParseIntPipe) id: number) {
+      return this.plantsService.findOne(id);
+    }
+  
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    create(@Body() dto: CreatePlantDto) {
+      return this.plantsService.create(dto);
+    }
+  
+    @Patch(':id')
+    update(
+      @Param('id', ParseIntPipe) id: number,
+      @Body() dto: UpdatePlantDto,
+    ) {
+      return this.plantsService.update(id, dto);
+    }
+  
+    @Delete(':id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    remove(@Param('id', ParseIntPipe) id: number) {
+      return this.plantsService.remove(id);
+    }
+  }
+  
